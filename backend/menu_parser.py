dfa495abf6c489f50839affbec75b8d6a7ea72a3
madelynshaffer <madelyn.shaffer@mail.mcgill.ca>
2023-01-28 14:20:41 -0500

    foo

--- backend/menu_parser.py
+++ backend/menu_parser.py
@@ -1,5 +1,9 @@
 from PyPDF2 import PdfReader
 import json
+import pandas as pd
+import warnings
+
+
 def read_text(path, page_number):
     '''(str, int) -> str'''
     reader = PdfReader(path)
@@ -9,9 +13,23 @@ def read_text(path, page_number):
         #menu_str += page.extract_text()
     return page.extract_text()
 
-#print(read_text("./menus/bmh_week1_2022.pdf", 0))
-print(read_text("./menus/rvc_week4_2022.pdf", 0))
-def create_database():
 
+#print(read_text("./menus/bmh_week1_2022.pdf", 0))
+#print(read_text("./menus/rvc_week4_2022.pdf", 0))
+#print(read_text("./menus/rvc_week3_2022.pdf", 0))
+
+
+def load_csv_file(filename):
+    #read and store content of the excel file
+    read_file = pd.read_excel(filename)
+    #write the df obj into a csv file
+    read_file.to_csv(filename, index=None, header=True)
+    #read a csv file and convert into a dataframe obj
+    df = pd.DataFrame(pd.read_csv(filename))
+    return df
+
+
+print(load_csv_file("./menus/rvc_week2_2022.xlsx"))
+def create_database():
     pass
 
