872f42de5579194b5ad1ceedf6cedfe9099aeb05
madelynshaffer <madelyn.shaffer@mail.mcgill.ca>
2023-01-28 13:41:51 -0500

    foo

--- backend/menu_parser.py
+++ backend/menu_parser.py
@@ -1,7 +1,17 @@
 from PyPDF2 import PdfReader
+import json
+def read_text(path, page_number):
+    '''(str, int) -> str'''
+    reader = PdfReader(path)
+    page = reader.pages[page_number]
+    #for page_num in range(len(reader.pages)):
+        #page = reader.pages[page_num]
+        #menu_str += page.extract_text()
+    return page.extract_text()
 
-reader = PdfReader("./menus/bmh_week1_2022.pdf")
+#print(read_text("./menus/bmh_week1_2022.pdf", 0))
+print(read_text("./menus/rvc_week4_2022.pdf", 0))
+def create_database():
 
-for page_num in range(len(reader.pages)):
-    page = reader.pages[page_num]
-    print(page.extract_text())
+    pass
+
