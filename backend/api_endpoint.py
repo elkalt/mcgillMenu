ffb3c7c513f12ae89b46d00b561c698d69342768
syranis <elliott.kalt@gmail.com>
2023-01-28 13:00:08 -0500

    add bmh.json example

--- backend/api_endpoint.py
+++ backend/api_endpoint.py
@@ -1,10 +1,14 @@
 from flask import Flask, make_response, request
-# $ flask run --host=0.0.0.0
+
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+@app.route("/api/status")
+def status():
+    return make_response({"status": "ok"})
 
 
-
+@app.route("/api/weekly/<int:week>")
+def api():
+    return make_response({"status": "ok"})
