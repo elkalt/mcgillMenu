749c691fe2ebdc3577b9c44bfd752b3094577112
Louis Meunier <louismeunier123@gmail.com>
2023-01-29 08:38:33 -0500

    Banana

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -14,5 +14,5 @@
 <p>This website was made over the course of the McHacks 10 hackathon by 
     <a href=https://www.linkedin.com/in/elliott-kalt-6565881b8>Elliott Kalt</a>,
     <a href=https://www.linkedin.com/in/louis-meunier-112b55203>Louis Meunier</a>,
-    and Madelyn Shaffer.
+    and <a target="_blank" rel="noreferrer" href="https://media.tenor.com/RQKAyM7ZHA0AAAAC/banana-dance.gif">Madelyn Shaffer</a>.
 </p>
