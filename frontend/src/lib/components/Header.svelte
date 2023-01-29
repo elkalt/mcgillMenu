e992a78bb81b2a36e24b00ac16bbd0f586c2ee5b
Louis Meunier <louismeunier123@gmail.com>
2023-01-29 11:44:39 -0500

    Title

--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -3,7 +3,12 @@
 </script>
 
 <header>
-    <h1>Better McGill Menu</h1>
+    <div class="title">
+        <h1>Campus</h1> 
+        <img src="bird.png" height="60px"/>
+        
+        <h1>Critics</h1>
+    </div>
     <nav>
         {#if defaultDisplay}
             <a href="/">Menu</a> |
@@ -18,12 +23,21 @@
 <style>
     header > h1 {
         color: var(--dark-pink);
+        font-size: 2.8em;
     }
     header {
         height: 100px;
         width: 100%;
         text-align: center;
-        padding-bottom: 5px;
+        padding-bottom: 20px;
         border-bottom: 2px solid var(--dark-pink);
+    }
+
+    .title {
+        display: flex;
+        flex-direction: row;
+        width: 100%;
+        justify-content: center;
+        align-items: center;
     }
 </style>
