ad2e8561f5e7208092524cc720877f28f35023c3
syranis <elliott.kalt@gmail.com>
2023-01-29 10:11:05 -0500

    idek

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,6 +3,11 @@
     import "/src/app.css";
 
     export let data;
+    data = data.data;
+    
+    const daysOfWeek = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+    console.log(data.data);
+
 </script>
 
 <Header defaultDisplay={ false }>
@@ -14,27 +19,25 @@
 <h2>This weeks menu:</h2>
 
 <div class="container">
-    <div class="item grid_header">Monday</div>
-    <div class="item grid_header">Tuesday</div>
-    <div class="item grid_header">Wednesday</div>
-    <div class="item grid_header">Thursday</div>
-    <div class="item grid_header">Friday</div>
-    <div class="item grid_header">Saturday</div>
-    <div class="item grid_header">Sunday</div>
-    {#each Object.keys(data.data) as dining_day}
-        {#if data.data[dining_day].menu.length > 0}
-            {#each Object.keys(data.data[dining_day].menu) as meal}
-                <div class="item">{ data.data[dining_day].menu[meal] }</div>
-            {/each}
-        {:else}
-            <div class="item">No menu available</div>
-            <div class="item">No menu available</div>
-            <div class="item">No menu available</div>
-            <div class="item">No menu available</div>
-            <div class="item">No menu available</div>
-            <div class="item">No menu available</div>
-            <div class="item">No menu available</div>
-        {/if}
+    <div></div>
+    {#each daysOfWeek as day}
+        <div class="item grid_header"><span style="text-transform: capitalize">{day}</span></div>
+    {/each}
+    <div class="item grid_header">Royal Victoria College</div>
+    <div class="item grid_header" style="grid-column: 3/4">Douglas Hall</div>
+    <div class="item grid_header" style="grid-column: 4/5">Bishop Mountain Hall</div>
+    <div class="item grid_header" style="grid-column: 5/6">Carrefour Sherbrooke</div>
+    <div class="item grid_header" style="grid-column: 6/7">New Residence Hall</div>
+    {#each Object.keys(data) as id}
+        {#each daysOfWeek as day}
+            {#if data[id].menu[day] > 0}
+                <div class="item">
+                    <!-- <a href={``}>{ data.data[dining_day].menu[meal] }</a> -->
+                </div>
+            {:else}
+                <div class="item">No menu available</div>
+            {/if}
+        {/each}
     {/each}
 </div>
 
@@ -42,12 +45,13 @@
 <style>
     .container {
         display: grid;
-        grid-template-columns: repeat(7, 1fr);
-        grid-template-rows: repeat(6, 1fr);
+        grid-auto-columns: minmax(6, 1fr);
+        grid-template-rows: repeat(8, 1fr);
         grid-gap: 1px;
         padding-left: 2rem;
         padding-right: 2rem;
         background-color: var(--background-color);
+        grid-auto-flow: column;
     }
     .item {
         font-family: 'Montserrat', sans-serif;
@@ -56,6 +60,7 @@
         background-color: var(--background-color-accent);
         border-radius: 5px;
         text-align: center;
+        justify-content: center;
         color: var(--secondary-color-accent2)
     }
     .grid_header {
--- frontend/src/routes/+page.ts
+++ frontend/src/routes/+page.ts
@@ -1,6 +1,6 @@
 import type { PageLoad } from './$types';
  
-export const load = (async ({ event, params }: any) => {
+export const load = (async () => {
     const response = await fetch(`https://louismeunier.pythonanywhere.com/api/dining_halls`);
     const response_json = await response.json();
     return response_json;
